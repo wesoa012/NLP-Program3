06e98aad6f9bc9229f91bffc4d29939708cb9d5c
wesoa012 <wesa@smu.edu>
2023-11-24 20:58:39 -0600

    Data Generation/Merger
    Run the DataReader.py if you want to add more stuff to the txt files

---
+++ DataReader.py
@@ -0,0 +1,36 @@
+from os import listdir
+def dataAdder():
+    for file in listdir('./Data/adds'):
+        if("Conviction" in file):
+            AddToFile = open("./Data/txts/Conviction.txt", "a")
+        elif("Thorn" in file):
+            AddToFile = open("./Data/txts/Thorn.txt", "a")
+        elif("Rubbish" in file):
+            AddToFile = open("./Data/txts/Rubbish.txt", "a")
+        else:
+            print("not for this program")
+        
+        ReadFromFile = open("./Data/adds/"+file, "r")
+        if("1.txt" in file):
+            sense = "1"
+        elif("2.txt" in file):
+            sense = "2"
+        else:
+            print("Extra File Detected...skipping it")
+            continue
+        AddToFile.write('\n')
+        for line in ReadFromFile.readlines():
+            AddToFile.write(sense + " " + line)
+        
+    
+    
+def dataToCSVs():
+    for file in listdir('./Data/txts'):
+        csv = open("./Data/csvs/" + file[0:-4] + ".csv", "w")
+        txt = open("./Data/txts/" + file, "r")
+        
+        for line in txt.readlines():
+            csv.write(line[0] + ',' + line[2:])
+    
+dataAdder()
+dataToCSVs()
