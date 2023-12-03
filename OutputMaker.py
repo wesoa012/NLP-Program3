7ffb77acee2dc2c3d6196a1c23a62c1b9c4e114a
kassi-bertrand <bertran.billy@gmail.com>
2023-12-02 19:17:14 -0600

    Renamed bert-fine-tuning to bert_fine_tuning

--- OutputMaker.py
+++ OutputMaker.py
@@ -1,3 +1,16 @@
+import os
+import sys
+import pandas as pd
+import torch
+import torch.nn.functional as F
+
+from transformers import DistilBertTokenizerFast
+from bert_fine_tuning.WSD.WSDDataset import WSDDataset
+from bert_fine_tuning.WSD.wsd_classifier import WSDClassifier
+
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+TOKENIZER = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+
 def WSD_Test_Rubbish(sentences):
     output = open("./rubbish_outputs.txt", "w")
     
